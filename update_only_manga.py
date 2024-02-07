8c71fe27ca58e2b237489d4eb3781fa67e297d51
Shiroe <maciejbielecki99@gmail.com>
2024-02-08 00:10:28 +0100

    added favorite updater as first api doesnt work

--- update_only_manga.py
+++ update_only_manga.py
@@ -464,6 +464,72 @@ Page(page: $page, perPage: $perPage) {
             
             total_added+= 1 
              
+
+    
+    page = 0
+    per_page_favorite = 25
+    has_next_page = True
+    connection = conn
+    cursor = connection.cursor()
+
+    # Fetch already favorite manga IDs to optimize updates
+    cursor.execute("SELECT id_anilist FROM manga_list WHERE is_favourite = 1")
+    already_favorites = {row[0] for row in cursor.fetchall()}
+
+    while has_next_page:
+        variables_in_api = {'page': page, 'id': user_id}
+        api_request = '''
+        query ($page: Int, $id: Int) {
+            User(id: $id) {
+                id
+                name
+                favourites {
+                    manga(page: $page) {
+                        pageInfo {
+                            total
+                            perPage
+                            currentPage
+                            lastPage
+                            hasNextPage
+                        }
+                        nodes {
+                            id
+                            title {
+                                english
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        '''
+        url = 'https://graphql.anilist.co'
+        response_from_anilist = requests.post(url, json={'query': api_request, 'variables': variables_in_api})
+        parsed_json = json.loads(response_from_anilist.text)
+
+        ids_to_update = [fav_manga["id"] for fav_manga in parsed_json["data"]["User"]["favourites"]["manga"]["nodes"] if fav_manga["id"] not in already_favorites]
+        print(f"{RED}Updating favorite fields.{RESET}") 
+        print(f"{RED}Page {page}{RESET}")
+        # If there are new favorites to update
+        if ids_to_update:
+            update_query = "UPDATE manga_list SET is_favourite = 1 WHERE id_anilist = %s"
+            try:
+                # Execute updates in a batch
+                for fav_manga_id in ids_to_update:
+                    cursor.execute(update_query, (fav_manga_id,))
+                # Commit all changes at once
+                connection.commit()
+                print(f"{GREEN}Updated {len(ids_to_update)} mangas to favorite{RESET}")
+            except mysql.connector.Error as err:
+                print(f"{RED}Failed to batch update mangas: {err}{RESET}")
+
+        has_next_page = parsed_json["data"]["User"]["favourites"]["manga"]["pageInfo"]["hasNextPage"]
+        page += 1
+    
+
+
+
+
     print(f"{YELLOW}Total added: {total_added}{RESET}")
     print(f"{MAGENTA}Total updated: {total_updated}{RESET}")
     conn.commit()
